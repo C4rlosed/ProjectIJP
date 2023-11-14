8a59e75114f6f3d11dee9248ba776587a36db3ef
Carlos Eduardo Pereira Bento <107080282+C4rlosed@users.noreply.github.com>
2023-11-14 10:56:21 -0300

    Att. pág de noticias (cards)

--- src/Noticias/index.js
+++ src/Noticias/index.js
@@ -6,20 +6,56 @@ import '../Noticias/noticia.css'
 const NoticiaContainer = style.div``
 
 
-function Noticia(){
-    return(
+function Noticia() {
+    return (
         <NoticiaContainer>
             <Header></Header>
-                <div className='noticias-bg'>
-                    <div className='noticias-banner'>
-                        <h1>HISTÓRIAS QUE MARCAM</h1>
-                        <p>VEJA AS NOSSAS NOTÍCIAS</p>
-                    </div>
+            <div className='noticias-bg'>
+                <div className='noticias-banner'>
+                    <h1>HISTÓRIAS QUE MARCAM</h1>
+                    <p>VEJA AS NOSSAS NOTÍCIAS</p>
                 </div>
-                <h2 className='titulo-pg'>VEJA MAIS</h2>
+            </div>
+            <h2 className='titulo-pg'>VEJA MAIS</h2>
+
+            <div className='card-container'>
+                <div className='card'>
+                    {data.map((d) => (
+                        <div>
+                            <div>
+                                <img src={d.img} alt="" />
+                            </div>
+                            <div>
+                                <h2>{d.title}</h2>
+                                <p>{d.info}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
             <Footer></Footer>
         </NoticiaContainer>
     )
 };
+
+const data = [
+    {
+        title: `PORTA EST ANTE`,
+        info: `In porta est ante, sed convallis turpis ornare et`,
+        img: '../Utilidades/Imagem/noticia1.png'
+    },
+    {
+        title: `QUISQUE ALIQUAM`,
+        info: `Quisque aliquam ligula et mi pharetra feugiat.`,
+        img: '../Utilidades/Imagem/noticia2.png'
+    },
+    {
+        title: `PORTA EST ANTE`,
+        info: `In porta est ante, sed convallis turpis ornare et`,
+        img: '../Utilidades/Imagem/noticia3.png'
+    },
+
+
+]
 
 export default Noticia;
