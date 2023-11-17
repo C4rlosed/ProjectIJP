482db5c1781cb63690792339c97d7968d5e23ea7
Carlos Eduardo Pereira Bento <107080282+C4rlosed@users.noreply.github.com>
2023-11-16 21:35:56 -0300

    At. inicio pág Parceiros

--- src/Parceiros/index.js
+++ src/Parceiros/index.js
@@ -1,13 +1,57 @@
 import style from 'styled-components'
 import Header from '../Utilidades/Header';
+import '../Parceiros/parceiros.css';
+import parceiroImg from '../Utilidades/Imagem/parceiros/parcImg.png'
+import movimentoconexao from '../Utilidades/Imagem/parceiros/movimentoconexao.png'
+import quebrandobarreiras from '../Utilidades/Imagem/parceiros/quebrandobarreiras.png'
+import lovefutbol from '../Utilidades/Imagem/parceiros/lovefutbol.png'
+import freefirelogo from '../Utilidades/Imagem/parceiros/freefirelogo.png'
 
-const ParceiroContainer = style.div``
+const ParceiroContainer = style.div`
+`
 
 
-function Parceiro(){
-    return(
+function Parceiro() {
+    return (
         <ParceiroContainer>
             <Header></Header>
+            <div className="parceiro-banner">
+                <h1><span id=""> </span></h1>
+                <p></p>
+            </div>
+
+            <div className="slider">
+                <div className="slide-track">
+                    <div className="slide">
+                        <img src={movimentoconexao} height="100" width="250"
+                            alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src={quebrandobarreiras} height="80" width="200"
+                            alt="" />
+                    </div>
+                    <div className="slide">
+                            <img src={lovefutbol} height="100" width="200"
+                            alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src={freefirelogo} height="100" width="200"
+                            alt="" />
+                    </div>
+                </div>
+            </div>
+
+            <section className="background">
+                <img src={parceiroImg} alt="" />
+                <div>
+                    <h2><span id='font'>UM POUCO DO QUE JÁ </span> CONSTRUIMOS JUNTOS </h2>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at
+                        its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+
+                    </p>
+                    <button type="button" className="button-parceiro">Saiba Mais</button>
+                </div>
+            </section>
         </ParceiroContainer>
     )
 };
