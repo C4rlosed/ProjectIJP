951dc89834af3d13e4913d6d8dfa9fecbb61efd9
Carlos Eduardo Pereira Bento <107080282+C4rlosed@users.noreply.github.com>
2023-11-23 20:21:09 -0300

    At. carrossel da pág Parceiros

--- src/Parceiros/carrossel.js
+++ src/Parceiros/carrossel.js
@@ -1,12 +1,11 @@
 import 'swiper/css';
 import 'swiper/css/bundle';
 import { Navigation, Pagination } from 'swiper/modules';
-import imgparceiros from '../Utilidades/Imagem//parceiros/quebrandobarreiras.png'
-import imgparceiros1 from '../Utilidades/Imagem//parceiros/quebrandobarreiras.png'
-import imgparceiros2 from '../Utilidades/Imagem//parceiros/quebrandobarreiras.png'
-import imgparceiros3 from '../Utilidades/Imagem//parceiros/quebrandobarreiras.png'
-import imgparceiros4 from '../Utilidades/Imagem//parceiros/quebrandobarreiras.png'
-import imgparceiros5 from '../Utilidades/Imagem//parceiros/quebrandobarreiras.png'
+import imgparceiros from '../Utilidades/Imagem//parceiros/carrossel/1.jpg'
+import imgparceiros1 from '../Utilidades/Imagem//parceiros/carrossel/2.jpg'
+import imgparceiros2 from '../Utilidades/Imagem//parceiros/carrossel/3.jpg'
+import imgparceiros3 from '../Utilidades/Imagem//parceiros/carrossel/4.jpg'
+import imgparceiros4 from '../Utilidades/Imagem//parceiros/carrossel/5.jpg'
 import '../Parceiros/parceiros.css'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import styled from 'styled-components';
@@ -14,45 +13,23 @@ import styled from 'styled-components';
 const data = [
     {
         id: '1',
-        title: `PORTA EST ANTE`,
-        info: `In porta est ante, sed convallis turpis ornare et`,
-        date: `21/08/2021`,
         image: imgparceiros
     },
     {
         id: '2',
-        title: `QUISQUE ALIQUAM`,
-        info: `Quisque aliquam ligula et mi pharetra feugiat.`,
-        date: `12/05/2021`,
         image: imgparceiros1
     },
     {
         id: '3',
-        title: `PORTA EST ANTE`,
-        info: `In porta est ante, sed convallis turpis ornare et`,
-        date: `08/11/2022`,
         image: imgparceiros2
     },
     {
         id: '4',
-        title: `PORTA EST ANTE`,
-        info: `In porta est ante, sed convallis turpis ornare et`,
-        date: `21/08/2021`,
         image: imgparceiros3   
     },
     {
         id: '5',
-        title: `QUISQUE ALIQUAM`,
-        info: `Quisque aliquam ligula et mi pharetra feugiat.`,
-        date: `12/05/2021`,
         image: imgparceiros4
-    },
-    {
-        id: '6',
-        title: `PORTA EST ANTE`,
-        info: `In porta est ante, sed convallis turpis ornare et`,
-        date: `08/11/2022`,
-        image: imgparceiros5
     },
 ]
 
@@ -75,11 +52,6 @@ function CarrosselParceiro() {
                 {data.map((d) => (
                     <SwiperSlide key={d.id}>
                         <img src={d.image} alt='Slider' className='slide-item' />
-                        <div className='slider-text'>
-                            <h2>{d.title}</h2>
-                            <p>{d.info}</p>
-                            <p>{d.date}</p>
-                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
